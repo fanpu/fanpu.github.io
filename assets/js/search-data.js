97e2df9444a608211bd19fc951b48cb8e339bac8
fanpu <fanpu@users.noreply.github.com>
2025-01-12 01:37:18 +0000

    Deploying to gh-pages from @ fanpu/fanpu.github.io@5680eabfa4d522d6e18b04027a923bda6fe86c60 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -655,52 +655,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-will-be-at-neurips-from-12-10-12-15-let-s-chat-if-you-re-also-there",
           title: 'I will be at NeurIPs from 12/10-12/15. Let’s chat if you’re also there!...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-graphical-bayesian-networks-for-predicting-asset-covariances",
+          section: "News",},{id: "projects-graphical-bayesian-networks-for-predicting-asset-covariances",
           title: 'Graphical Bayesian Networks for Predicting Asset Covariances',
           description: "Covariance matrix prediction is a long-standing challenge in modern portfolio theory and quantitative finance. In this project, we investigate the effectiveness of Bayesian networks in predicting the covariance matrix of financial assets (specifically a subset of the S&amp;P 500), evaluated against Heterogeneous Autoregressive (HAR) models.",
           section: "Projects",handler: () => {
